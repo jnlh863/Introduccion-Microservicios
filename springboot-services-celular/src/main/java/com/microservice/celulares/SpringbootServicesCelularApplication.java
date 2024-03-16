ca0af3e3a577cd8c28f78440deee55c71a8f85b1
jnlh863 <lopezhernandezjahirnicolas@gmail.com>
2024-03-16 16:31:14 -0600

    Proyecto-Spring-Tool-Suite

---
+++ springboot-services-celular/src/main/java/com/microservice/celulares/SpringbootServicesCelularApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.celulares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicesCelularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicesCelularApplication.class, args);
+	}
+
+}
---
+++ springboot-services-store/src/main/java/com/microservice/store/controller/StoreController.java
@@ -0,0 +1,31 @@
+package com.microservice.store.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.store.models.Store;
+import com.microservice.store.service.StoreService;
+
+@RestController
+public class StoreController {
+	
+	@Autowired
+	//@Qualifier("serviceRest")
+	private StoreService StoreService;
+		
+	@GetMapping("/list")
+	public List<Store> list(){
+		return StoreService.findAll();
+	}
+	
+	@GetMapping("/store/{id}/cantidad/{cantidad}")
+	public Store details(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return StoreService.findById(id, cantidad);
+	}
+	
+}
